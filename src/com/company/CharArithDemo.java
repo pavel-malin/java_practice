877d4342518ac5105b9d891fa43808a698c4aa16
pavel-malin <kurchevskijpavel@gmail.com>
2020-02-27 23:17:44 +0300

    char, int, double, float

---
+++ src/com/company/CharArithDemo.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'X';
+        System.out.println("ch = " + ch);
+
+        ch++; // Y
+        System.out.println(ch);
+
+        ch = 90; // Z
+        System.out.println(ch);
+
+    }
+}
---
+++ src/com/company/Inches.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Inches {
+    public static void main(String[] args) {
+        long ci, im;
+
+        im = 5280 * 12;
+
+        ci = im * im * im;
+
+        System.out.println("1 cube mills " + ci + " cube inches");
+    }
+}
