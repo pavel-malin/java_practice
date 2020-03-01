961176a336d41983cee502bd906790453de29447
pavel-malin <kurchevskijpavel@gmail.com>
2020-03-01 19:04:40 +0300

    guess, guess1

---
+++ src/com/company/Guess.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Guess {
+    public static void main(String[] args)
+        throws java.io.IOException {
+
+        char ch, answer = 'K';
+
+        System.out.println("Designed a letter from the range A-Z.");
+        System.out.print("Try to guess her: ");
+
+        ch = (char) System.in.read();
+
+        if (ch == answer)
+            System.out.println("*** OK ***");
+    }
+}
