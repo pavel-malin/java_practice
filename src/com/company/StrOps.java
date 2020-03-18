2890828361ef51623b3106f1209722f78a8e4552
pavel-malin <kurchevskijpavel@gmail.com>
2020-03-18 20:59:57 +0300

    foreach2, search, stringdemo, strops.

---
+++ src/com/company/StrOps.java
@@ -0,0 +1,43 @@
+package com.company;
+
+class StrOps {
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = new String(str1);
+        String str3 = "Strings Java";
+        int result, idx;
+        char ch;
+
+        System.out.println("Len str1: " + str1.length());
+
+        for (int i = 0; i < str1.length(); i++)
+            System.out.println(str1.charAt(i));
+        System.out.println();
+
+        if(str1.equals(str2))
+            System.out.println("str1 instance str2");
+        else
+            System.out.println("str1 no is instance str2");
+
+        if (str1.equals(str3))
+            System.out.println("str1 instance str3");
+        else
+            System.out.println("str1 no instance str3");
+
+        result = str1.compareTo(str3);
+        if (result == 0)
+            System.out.println("str1 and str3 equally");
+        else if (result < 0)
+            System.out.println("str1 smaller str3");
+        else
+            System.out.println("str1 big's str3");
+
+        str2 = "One Two Three One";
+
+        idx = str2.indexOf("One");
+        System.out.println("Index one's is included One: " + idx);
+        idx = str2.lastIndexOf("One");
+        System.out.println("Index last's is included One: " + idx);
+
+    }
+}
---
+++ src/com/company/StringDemo.java
@@ -0,0 +1,13 @@
+package com.company;
+
+class StringDemo {
+    public static void main(String[] args) {
+        String str1 = new String("str1");
+        String str2 = "str2";
+        String str3 = new String(str2);
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
