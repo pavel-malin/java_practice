b576d32d7175ef33445356021da346d12c30f76f
pavel-malin <kurchevskijpavel@gmail.com>
2020-03-19 13:12:16 +0300

    string switch

---
+++ src/com/company/StringSwitch.java
@@ -0,0 +1,22 @@
+package com.company;
+
+class StringSwitch {
+    public static void main(String[] args) {
+        String command = "cancel";
+
+        switch (command) {
+            case "connect":
+                System.out.println("Connect");
+                break;
+            case "cancel":
+                System.out.println("Cancel");
+                break;
+            case "disconnect":
+                System.out.println("Disconnect");
+                break;
+            default:
+                System.out.println("Invalid team!");
+                break;
+        }
+    }
+}
