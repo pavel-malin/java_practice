1e2f91ac06e60d98c507ad6652110bd9b355c8f6
pavel-malin <kurchevskijpavel@gmail.com>
2020-03-23 21:30:03 +0300

    callbyvalue.

---
+++ src/com/company/CallByValue.java
@@ -0,0 +1,22 @@
+package com.company;
+
+class Test {
+    void noChange(int i, int j) {
+        i = i + j;
+        j = -j;
+    }
+}
+
+class CallByValue {
+    public static void main(String[] args) {
+        Test ob = new Test();
+
+        int a = 15, b = 20;
+
+        System.out.println("a and b before calling: " + a + " " + b);
+
+        ob.noChange(a, b);
+
+        System.out.println("a and b after calling: " + a + " " + b);
+    }
+}
