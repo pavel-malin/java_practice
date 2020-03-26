a8b2b301a843c2e3b8668cdefe450fd43e4346a9
pavel-malin <kurchevskijpavel@gmail.com>
2020-03-26 10:50:13 +0300

    sdemo.

---
+++ src/com/company/SDemo.java
@@ -0,0 +1,37 @@
+package com.company;
+
+class StaticDemo {
+    int x;
+    static int y;
+
+    int sum() {
+        return x + y;
+    }
+}
+
+class SDemo {
+    public static void main(String[] args) {
+        StaticDemo ob1 = new StaticDemo();
+        StaticDemo ob2 = new StaticDemo();
+
+        ob1.x = 10;
+        ob2.x = 20;
+
+        System.out.println("Of course, ob1.x and ob2.x " + "are independent");
+        System.out.println("ob1.x: " + ob1.x + "\nob2.x: " + ob2.x);
+        System.out.println();
+
+        System.out.println("Static variable y - general");
+
+        StaticDemo.y = 19;
+        System.out.println("Assign StaticDemo.y value 19");
+        System.out.println("ob1.sum(): " + ob1.sum());
+        System.out.println("ob2.sum(): " + ob2.sum());
+        System.out.println();
+
+        StaticDemo.y = 100;
+        System.out.println("Change the value StaticDemo.y to 100");
+        System.out.println("ob1.sum(): " + ob1.sum());
+        System.out.println("ob2.sum(): " + ob2.sum());
+    }
+}
