3f7c9c3640a4cde6b3848b43c74465f26c17244a
malinroot <kurchevskijpavel@gmail.com>
2020-06-26 21:56:22 +0300

    new NotHandled

---
+++ src/NotHandled.java
@@ -0,0 +1,7 @@
+public class NotHandled {
+    public static void main(String[] args) {
+        int nums[] = new int[4];
+        System.out.println("Before throwing an exception");
+        nums[7] = 10;
+    }
+}
