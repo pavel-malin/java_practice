3ebe6bb92f6bd57c902dbe6224f4abfebf07b424
malinroot <kurchevskijpavel@gmail.com>
2020-06-26 19:54:03 +0300

    SericesDemo.java new default int[] getnextarray

--- src/com/company/SericesDemo.java
+++ src/com/company/SericesDemo.java
@@ -4,6 +4,14 @@ interface Series {
     int getNext();
     void reset();
     void setStart(int x);
+
+    default int[] getNextArray(int n) {
+        int[] vals = new int[n];
+
+        for(int i=0; i<n; i++)
+            vals[i] = getNext();
+        return vals;
+    }
 }
 
 class ByTows implements Series {
