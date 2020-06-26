4e7eb13c45bc8b44a269eb06515f8b9008ca3729
malinroot <malinroot@192.168.100.7>
2020-06-26 19:00:06 +0300

    IConstD..

--- src/IConstD.java
+++ src/IConstD.java
@@ -1,2 +1,20 @@
-public class IConstD {
+interface IConst {
+    int MIN = 0;
+    int MAX = 10;
+    String ERRORS = "Range error";
+}
+
+class IConstD implements IConst {
+    public static void main(String[] args) {
+        int nums[] = new int[MAX];
+
+        for(int i = MIN; i<11; i++) {
+            if(i > MAX)
+                System.out.println(ERRORS);
+            else {
+                nums[i] = i;
+                System.out.print(nums[i] + " ");
+            }
+        }
+    }
 }
