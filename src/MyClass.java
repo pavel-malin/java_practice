d56f83348d06f054bef1e0daed87930ae1a6f885
malinroot <malinroot@192.168.100.7>
2020-06-26 18:41:29 +0300

    MyClass.java.

---
+++ src/MyClass.java
@@ -0,0 +1,32 @@
+
+interface A {
+    void meth1();
+    void meth2();
+}
+
+interface B extends A {
+    void meth3();
+}
+
+class MyClass implements B {
+    public void meth1() {
+        System.out.println("Implementation method meth1().");
+    }
+
+    public void meth2() {
+        System.out.println("Implementation method meth2().");
+    }
+
+    public void meth3() {
+        System.out.println("Implementation method meth3().");
+    }
+}
+
+class IFExtend {
+    public static void main(String[] args) {
+        MyClass ob = new MyClass();
+        ob.meth1();
+        ob.meth2();
+        ob.meth3();
+    }
+}
