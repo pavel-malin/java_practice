850c9762f6e9c217eaa98be4190ec85e13952443
malinroot <kurchevskijpavel@gmail.com>
2020-06-26 23:21:58 +0300

    new ThrowDemo

---
+++ src/ThrowDemo.java
@@ -0,0 +1,12 @@
+public class ThrowDemo {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Before the throw statement");
+            throw new ArithmeticException();
+        }
+        catch(ArithmeticException exc) {
+            System.out.println("Exception caught");
+        }
+        System.out.println("After try/catch block");
+    }
+}
