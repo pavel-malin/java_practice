e80b8810370c3f6e9444138f34dd4057390b0b95
malinroot <kurchevskijpavel@gmail.com>
2020-06-29 09:18:22 +0300

    new MultiCatch

---
+++ src/MultiCatch.java
@@ -0,0 +1,20 @@
+public class MultiCatch {
+    public static void main(String[] args) {
+        int a = 88, b = 0;
+        int result;
+        char chrs[] = {'A', 'B', 'C'};
+
+        for (int i=0; i<2; i++) {
+            try {
+                if(i == 0)
+                    result = a/b;
+                else
+                    chrs[5] = 'X';
+            }
+            catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+                System.out.println("Caught exception: " + e);
+            }
+        }
+        System.out.println("After the group exception interceptor");
+    }
+}
