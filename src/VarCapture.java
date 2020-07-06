706d57b095b15238612d9a0ca56006bfe3bf7721
malinroot <kurchevskijpavel@gmail.com>
2020-07-06 14:38:59 +0300

    new VarCapture

---
+++ src/VarCapture.java
@@ -0,0 +1,14 @@
+interface MyFunc {
+    int func(int n);
+}
+
+public class VarCapture {
+    public static void main(String[] args) {
+        int num = 10;
+        MyFunc myLambda = (n) -> {
+          int v = num + n;
+          return v;
+        };
+        System.out.println(myLambda.func(8));
+    }
+}
