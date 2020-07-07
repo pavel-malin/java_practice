9de52fca07494ed0571fac6dce893125c44be62a
malinroot <kurchevskijpavel@gmail.com>
2020-07-07 19:34:39 +0300

    new InchToMeterTable

---
+++ src/InchToMeterTable.java
@@ -0,0 +1,18 @@
+class InchToMeterTable {
+    public static void main(String[] args) {
+        double inches, meters;
+        int counter;
+        counter = 0;
+        
+        for (inches=1; inches<=144; inches++) {
+            meters = inches * 39.37;
+            System.out.println(inches + " Inches equals " + meters + " meters");
+            counter++;
+
+            if (counter == 12) {
+                System.out.println();
+                counter = 0;
+            }
+        }
+    }
+}
